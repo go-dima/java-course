6afd4073592abad32928eb1674c9a732cdb47e45
Dima Gonikman <dima.gonikman@gmail.com>
2019-03-26 14:48:40 +0200

    Add PersonDao
    - Inject entityManager and implement api
    - Use PersonDao in SpringJpaApplication.main(...)

--- spring-jpa/src/main/java/com/example/jpa/springjpa/SpringJpaApplication.java
+++ spring-jpa/src/main/java/com/example/jpa/springjpa/SpringJpaApplication.java
@@ -1,11 +1,21 @@
 package com.example.jpa.springjpa;
 
+import com.example.jpa.springjpa.db.Person;
+import com.example.jpa.springjpa.db.PersonDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringJpaApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJpaApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringJpaApplication.class, args);
+
+		PersonDao personDao = ctx.getBean(PersonDao.class);
+		Person p1 = new Person("Dima", 29);
+		long idP1 = personDao.addPerson(p1);
+		System.out.printf("Created person with id %d\n", idP1);
+		Person p2 = personDao.getPerson(idP1);
+		System.out.println("Got: " + p2);
 	}
 }
---
+++ spring-jpa/src/main/java/com/example/jpa/springjpa/db/PersonDao.java
@@ -0,0 +1,25 @@
+package com.example.jpa.springjpa.db;
+
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@NoArgsConstructor
+public class PersonDao {
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public long addPerson(Person p){
+        entityManager.persist(p);
+        return p.getId();
+    }
+
+    public Person getPerson(long id) {
+        return entityManager.find(Person.class, id);
+    }
+}
