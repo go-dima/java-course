e7eeae24285e3be96996d7f08723bdf629c2c8f1
Dima Gonikman <dima.gonikman@gmail.com>
2019-05-07 16:17:07 +0300

    Add hystrix dashboard to zuul gateway server
    - Test on http://localhost:8080/hystrix/monitor?stream=http%3A%2F%2Flocalhost%3A8080%2Fhystrix.stream&title=GatewayMonitor

--- cloud/zuul-proxy-server/src/main/java/com/example/zuulserver/ZuulServerApplication.java
+++ cloud/zuul-proxy-server/src/main/java/com/example/zuulserver/ZuulServerApplication.java
@@ -3,11 +3,13 @@ package com.example.zuulserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableHystrixDashboard
 public class ZuulServerApplication {
 	// https://localhost:9090
 	public static void main(String[] args) {
