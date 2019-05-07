e7c6ee31cd44d9462bf6a6c5e39d296ec90f3e7e
Dima Gonikman <dima.gonikman@gmail.com>
2019-05-07 14:21:51 +0300

    Add instanceId display to PaymentController

--- cloud/payment-service/src/main/java/com/example/paymentservice/springpaymentservice/PaymentController.java
+++ cloud/payment-service/src/main/java/com/example/paymentservice/springpaymentservice/PaymentController.java
@@ -1,13 +1,17 @@
 package com.example.paymentservice.springpaymentservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
 
+    @Value("${eureka.instance.instanceId}")
+    private  String instanceId;
+
     @GetMapping("/paymentservice")
     public String paymentService() {
-        return "Payment Service";
+        return "Payment Service #" + this.instanceId;
     }
 }
