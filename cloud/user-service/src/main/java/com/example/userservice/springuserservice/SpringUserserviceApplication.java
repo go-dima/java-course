4302f2a0c409dc35fcdddffa523b22ad10f584f6
Dima Gonikman <dima.gonikman@gmail.com>
2019-05-07 15:34:40 +0300

    Add hystrix for circuit breaking and fallback
    - Test on http://localhost:8001/userservice/pay
    - Added /pay api with redirect to payment-service and fallback

--- cloud/user-service/src/main/java/com/example/userservice/springuserservice/SpringUserserviceApplication.java
+++ cloud/user-service/src/main/java/com/example/userservice/springuserservice/SpringUserserviceApplication.java
@@ -2,14 +2,24 @@ package com.example.userservice.springuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class SpringUserserviceApplication {
 
 	public static void main(String[] args) {
-		// http://localhost:8001/userservice
 		SpringApplication.run(SpringUserserviceApplication.class, args);
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate testTemplate(){
+		return new RestTemplate();
 	}
 }
--- cloud/user-service/src/main/java/com/example/userservice/springuserservice/UserController.java
+++ cloud/user-service/src/main/java/com/example/userservice/springuserservice/UserController.java
@@ -1,13 +1,35 @@
 package com.example.userservice.springuserservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/userservice")
     public String userService() {
+        // http://localhost:8001/userservice
         return "User Service";
+    }
+
+    @HystrixCommand(fallbackMethod = "fallback",
+                    groupKey = "userservice",
+                    commandKey = "userservice",
+                    threadPoolKey = "userserviceThread")
+    @GetMapping("/userservice/pay")
+    public String Pay() {
+        // http://localhost:8001/userservice/pay
+        String url = "http://payment-service/paymentservice";
+        return restTemplate.getForObject(url, String.class) + " from UserService /userservice/pay";
+    }
+
+    public String fallback(Throwable hystrixCommand) {
+        return "Fallback - No available payment-service instances";
     }
 }
